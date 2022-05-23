63eac4481a4a8e6507574de5b18d9e7cc449b783
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-05-23 17:09:02 +0300

    AAdd the case where the message hasn't been read yet and renaming a variable

--- post_office.py
+++ post_office.py
@@ -31,12 +31,31 @@ class PostOffice:
             'title': title_of_message,
             'body': message_body,
             'sender': sender,
+            'is_read': False,
         }
         if urgent:
             user_box.insert(0, message_details)
         else:
             user_box.append(message_details)
         return self.message_id
+
+    def read_message(self, user_name, message):
+        """
+        A function that returns a message and updates that a message has been read.
+        :param user_name:Name of the user who received the message.
+        :param message:The message to read.
+        :return:The message.
+        """
+        self.boxes[user_name][message]['is_read'] = True
+        return {self.boxes[user_name][message]['title']: self.boxes[user_name][message]['body']}
+
+    def amount_of_messages_read(self, user_name):
+        """
+        A function that returns the amount of messages that have already been read.
+        :param user_name: Name of the user who received the messages.
+        :return: The amount of messages that have already been read.
+        """
+        return sum(1 for message in range(0, len(self.boxes[user_name]))if self.boxes[user_name][message]['is_read'])
 
     def read_inbox(self, user_name: str, n: int = None) -> list:
         """
@@ -45,17 +64,22 @@ class PostOffice:
         :param n:The number of first messages returned, if not passed to a function will return all user messages.
         :return:List of messages
         """
-        return [{self.boxes[user_name][i]['title']: self.boxes[user_name][i]['body']}
-                for i in range(0, len(self.boxes[user_name]) if n is None else n)]
+        if n >= len(self.boxes[user_name]):
+            return[{self.boxes[user_name][message]['title']: self.boxes[user_name][message]['body']}
+                   for message in range(0, len(self.boxes[user_name]))]
+        return [self.read_message(user_name, message)
+                for message in range(0, len(self.boxes[user_name]) if n is None
+                else n+self.amount_of_messages_read(user_name))
+                if not self.boxes[user_name][message]['is_read'] or n is None]
 
-    def search_inbox(self, user_name: str, string: str) -> list:
+    def search_inbox(self, user_name: str, text_message: str) -> list:
         """
         A function that returns a list of messages that contain the string it gets for the user it gets.
         :param user_name:Username for searching his messages.
-        :param string:Search string in body and title of messages.
+        :param text_message:Search string in body and title of messages.
         :return:A list of messages in which the string appears.
         """
-        return [{i['title']:i['body']} for i in self.boxes[user_name]if string in i['title'] or string in i['body']]
+        return [{message['title']:message['body']} for message in self.boxes[user_name]if text_message in message['title'] or text_message in message['body']]
 
 
 if __name__ == '__main__':
@@ -65,7 +89,8 @@ if __name__ == '__main__':
     new_post_office.send_message('Ortal', 'Dana', "Question", "Where are you?", urgent=False)
     new_post_office.send_message('Dana', 'Ortal', "Answer", "I'm home", urgent=False)
     new_post_office.send_message('Ortal', 'Dana', "Fire engine location", "Fire station", urgent=False)
+    new_post_office.send_message('Dana', 'Ortal', "Answer", "Bye", urgent=False)
 
     print(new_post_office.read_inbox('Ortal', 1))
-    print(new_post_office.read_inbox('Dana'))
-    print(new_post_office.search_inbox('Dana', 'tion'))
+    print(new_post_office.read_inbox('Ortal', 2))
+    print(new_post_office.read_inbox('Ortal', 10))
